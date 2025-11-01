1f2c0e1d6886c8f3f04516bc5bc0e9a378a35d91
boydclifton <bstinnett65@gmail.com>
2025-11-01 17:22:44 -0400

    changed the location of the folder

---
+++ scripts/navigation.js
@@ -0,0 +1,10 @@
+// Store the selected elements that we are going to use // 
+const navButton = document.querySelector("#ham-btn");
+const navLinks = document.querySelector("#nav-bar");
+
+
+navButton.addEventListener("click", () => {
+    navButton.classList.toggle("show");
+    navLinks.classList.toggle("show");
+
+});
