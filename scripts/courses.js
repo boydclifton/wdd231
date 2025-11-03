ad125b4ce06225897f5635571d6f56cfa3a86fe3
boydclifton <bstinnett65@gmail.com>
2025-11-03 12:39:16 -0500

    changed up course card stylings

--- scripts/courses.js
+++ scripts/courses.js
@@ -97,9 +97,10 @@ function renderCourses(courseList) {
         }
 
         card.innerHTML = `
-        <h3>${course.subject} ${course.number}: ${course.title}    ${course.completed ? "✅" : "❌"} </h3>
+        <h3>${course.completed ? "✅" : "❌"}          ${course.subject} ${course.number}: ${course.title}    
+        </h3>
         
-        <p class="status"></p> 
+        
         `;
 
         coursesContainer.appendChild(card);
